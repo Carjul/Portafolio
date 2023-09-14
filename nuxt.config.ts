feff08aa6054a1ea4d8966f3c20d7db8c60647b0
Carjul <emugameplay.tv@gmail.com>
2023-09-13 23:12:46 -0500

    ...

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  modules: ['@sidebase/nuxt-pdf'],
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
