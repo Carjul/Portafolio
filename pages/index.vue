c8659d2f513bbf9abb3e0b9217107af09a576afe
Carlos Ramos González <emugameplay.tv@gmail.com>
2024-08-15 11:11:26 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -100,6 +100,15 @@ function nextPageProject() {
                         Resume
                     </a>
                 </li>
+                 <li>
+                    <a class="bg-primary from-cyan-500 text-to-teal-500 text-white px-2 py-2 border-none rounded-md ml-8 transition-all duration-300 ease-in-out hover:bg-gradient-to-r hover:from-red-500 hover:to-orange-500"
+                       href="https://github.com/Carjul" target="_blank">
+                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current">
+                        <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z">
+                        </path>
+                    </svg>
+                    </a>
+                </li>
             </ul>
         </nav>
     </section>
