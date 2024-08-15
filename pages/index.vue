a736787dcc3d6577c3e08e0c6109f40fac9cad32
Carlos Ramos González <emugameplay.tv@gmail.com>
2024-08-15 11:24:15 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -94,13 +94,13 @@ function nextPageProject() {
         <nav class="mt-10 flex items-center">
             <h1 class="font-burtons text-xl text-neutral-content">Developed by</h1>
             <ul class="flex items-center">
-                <li class="mx-10">
+                <li class="mx-5">
                     <a class="bg-primary from-cyan-500 text-to-teal-500 text-white px-2 py-2 border-none rounded-md ml-8 transition-all duration-300 ease-in-out hover:bg-gradient-to-r hover:from-red-500 hover:to-orange-500"
                        href="https://www.linkedin.com/in/carlos-juli%C3%A1n-ramos/" target="_blank">
                         Resume
                     </a>
                 </li>
-               <li class="mx-10">
+               <li class="mx-5">
     <a  href="https://github.com/Carjul" target="_blank">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current">
             <path d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.438 9.8 8.205 11.385.6.11.82-.26.82-.577v-2.2c-3.338.726-4.042-1.61-4.042-1.61-.546-1.385-1.333-1.755-1.333-1.755-1.09-.744.083-.729.083-.729 1.204.085 1.838 1.236 1.838 1.236 1.07 1.835 2.806 1.305 3.492.997.108-.775.42-1.306.763-1.607-2.665-.305-5.466-1.333-5.466-5.93 0-1.31.467-2.38 1.235-3.22-.124-.303-.536-1.523.117-3.176 0 0 1.008-.322 3.3 1.23a11.495 11.495 0 0 1 3.006-.404c1.02.004 2.044.138 3.006.404 2.29-1.552 3.297-1.23 3.297-1.23.655 1.653.243 2.873.12 3.176.77.84 1.233 1.91 1.233 3.22 0 4.61-2.804 5.622-5.476 5.92.43.37.823 1.102.823 2.222v3.293c0 .319.22.694.824.576C20.565 21.798 24 17.3 24 12 24 5.37 18.63 0 12 0z"></path>
