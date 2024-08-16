35391307ccccc2e68b431acedaf1a564005971a3
Carlos Ramos González <emugameplay.tv@gmail.com>
2024-08-16 03:55:55 +0000

    ...

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,20 +1,21 @@
 
 <template >
     <form @submit.prevent="submitContactForm" class="form flex flex-col w-4/5 mx-auto mb-10 mt-10 ">
-      
+      <label class="input input-bordered flex items-center gap-2">
+        *
         <input v-model="form.name" type="text"  placeholder="Nombre"  
         class="input h-full w-full " required />
-      
+      </label>
       <label class="input input-bordered flex items-center gap-2">
-        
+        *
         <input v-model="form.email" type="email"  placeholder="Email"  class="input h-full w-full " required />
       </label>
       <label class="input input-bordered flex items-center gap-2">
-        
+        *
         <input v-model="form.subject" type="text"  placeholder="Asunto" class="input h-full w-full " required />
       </label>
       <label class="input input-bordered flex items-center h-auto ">
-        
+        *
         <textarea v-model="form.message"  placeholder="Mensaje" class="textarea w-full px-4"  required></textarea>
       </label>
       <button type="submit" class="btn btn-primary mt-4">Enviar</button>
